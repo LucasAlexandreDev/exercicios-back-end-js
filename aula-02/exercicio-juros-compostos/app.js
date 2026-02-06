f439063c7d43ffe985f4fdb58540ca753057b575
LUCAS ALEXANDRE DA SILVA <25203702@CFP127ALUNO.LOCAL>
2026-02-06 16:45:00 -0300

    refactor: adiciona correções de lógica na estrutura do código

--- aula-02/exercicio-juros-compostos/app.js
+++ aula-02/exercicio-juros-compostos/app.js
@@ -18,69 +18,69 @@ const entradaDeDados = readline.createInterface({
 })
 
 // entrada de dados
-entradaDeDados.question('Digite o nome do(a) cliente: ', function(nome){
+entradaDeDados.question('Digite o nome do(a) cliente : ', function(nome){
     let nomeCliente = nome
 
-    entradaDeDados.question('Digite o nome do produto:', function(produto){
+    entradaDeDados.question('Digite o nome do produto :', function(produto){
         let nomeProduto = produto
     
-        entradaDeDados.question('Digite o valor da compra:', function(valor){
+        entradaDeDados.question('Digite o valor da compra :', function(capitalInicial){
             // Converte o valor do produto de string para número
-            let valorCompra = Number(valor)
+            let c = Number(capitalInicial)
 
-            entradaDeDados.question('Digite a taxa de juros:', function(juros){
+            entradaDeDados.question('Digite a taxa de juros :', function(taxajuros){
                 // Converte a taxa de juros de string para número
-                let taxaJuros = Number(juros)
+                let i = Number(taxajuros)
 
-                entradaDeDados.question('Digite o período do tempo de parcelamento:\n(1) meses\n(2) anos', function(periodo){
+                entradaDeDados.question('Digite o período do tempo de parcelamento (1) meses ou (2) anos : ', function(periodo){
                     // Converte o perído de parcelamento de string para número
                     let tempo = Number(periodo)
 
-                    entradaDeDados.question('Digite o tempo de parcelamento:', function(meses){
+                    entradaDeDados.question('Digite o tempo de parcelamento :', function(meses){
                         // Converte os meses de parcelamento de string para número
-                        let parcela = Number(meses)
+                        let n = Number(meses)
 
                     // validação da entrada de dados 
-                    if (nomeCliente == '' || nomeProduto == ''){
-                        console.log('Erro: Preencha todos os campos antes de continuar')
+                        if (nomeCliente == '' || nomeProduto == ''){
+                            console.log('Erro: Preencha todos os campos antes de continuar')
 
-                    }else if(!isNaN(nomeCliente)){
-                        console.log('Erro: O campo "nome do(a) cliente" não aceita números')
+                        }else if(!isNaN(nomeCliente)){
+                            console.log('Erro: O campo "nome do(a) cliente" não aceita números')
 
-                    }else if(isNaN(valorCompra) || isNaN(taxaJuros) || isNaN(parcela)){
-                        console.log('Erro: Os campos aceitam somente números')
+                        }else if(isNaN(c) || isNaN(i) || isNaN(n)){
+                            console.log('Erro: Os campos aceitam somente números')
 
-                    }else if(valorCompra <= 0 || taxaJuros <= 0 || parcela <= 0){
-                        console.log('Erro: Os campo não aceitam números menores ou iguais que 0 ')
-                        
-                    }else if (isNaN(tempo) || tempo != 1 && tempo != 2){
-                        console.log('Erro: O campo aceita somente o número 1 (meses) ou número 2 (anos)')
+                        }else if(c <= 0 || i <= 0 || n <= 0){
+                            console.log('Erro: Os campo não aceitam números menores ou iguais que 0 ')
+                            
+                        }else if (isNaN(tempo) || tempo != 1 && tempo != 2){
+                            console.log('Erro: O campo aceita somente o número 1 (meses) ou número 2 (anos)')
 
-                    }else{
+                        }else{
 
-                        // desafio bem legal - tempo de parcela por anos
-                        if (tempo === 2){
-                            parcela = parcela * 12
-                            taxaJuros = taxaJuros / 12
+                            // desafio bem legal - tempo de parcelamento por anos
+                            if (tempo == 2){
+                                n = n * 12
+                            }
+
+                            // conversão da taxa de juros percentual pra decimal 
+                            i = i / 100
+
+                            // cálculo do juros compostos | ** = potenciação
+                            const montante = c*(1 + i)**n
+
+                            let acrescimo = montante - c  
+
+                            // estrutura da saída de dados
+                            console.log(`\n******************* Viva Moda *******************\n\nMuito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.\nA compra do produto ${nomeProduto}, tem um valor de: R$ ${c}.\nA sua compra será parcelada em ${n} vezes e o Sr(a) pagará: R$ ${montante.toFixed(2)}.\nnO acréscimo realizado ao valor de: R$ ${c} será de R$ ${acrescimo.toFixed(2)}.\n\nMuito obrigado por escolher a Viva Moda.\n**************************************************`)
                         }
 
-                        taxaJuros = taxaJuros / 100
-
-                        let c = valorCompra
-                        let i = taxaJuros
-                        let n = parcela
-
-                        // cálculo do juros compostos | ** = potenciação
-                        const montante = c*(1 + i)**n
-
-                        let acrescimo = montante - valorCompra  
-
-                        // estrutura da saída de dados
-                        console.log(`\n******************* Viva Moda *******************\nMuito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.\nA compra do produto ${nomeProduto}, tem um valor de: R$ ${valorCompra}.\nA sua compra será parcelada em ${parcela} vezes e o Sr(a) pagará: R$ ${montante.toFixed(2)}.\nO acréscimo realizado ao valor de: R$ ${valorCompra} será de R$ ${acrescimo.toFixed(2)}.\n\nMuito obrigado por escolher a Viva Moda.\n*******************************************************`)
-                    }
+                        // O objeto que finaliza o arquivo
+                        entradaDeDados.close()
                     })   
                 }) 
             })
         })
     })
 })
+
