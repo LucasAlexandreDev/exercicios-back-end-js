d92862f6918f189fdaf44552df0990e0d713be70
Lucas Alexandre <lucasalexandre.lads13@gmail.com>
2026-02-06 11:27:45 -0300

    feat: adiciona validação do período de parcelamento + conversão de dados

--- aula-02/exercicio-juros-compostos/app.js
+++ aula-02/exercicio-juros-compostos/app.js
@@ -25,19 +25,23 @@ entradaDeDados.question('Digite o nome do(a) cliente: ', function(nome){
         let nomeProduto = produto
     
         entradaDeDados.question('Digite o valor da compra:', function(valor){
-            let valorCompra = valor
+            // Converte o valor do produto de string para número
+            let valorCompra = Number(valor)
 
             entradaDeDados.question('Digite a taxa de juros:', function(juros){
-                let taxaJuros = juros
+                // Converte a taxa de juros de string para número
+                let taxaJuros = Number(juros)
 
                 entradaDeDados.question('Digite o período do tempo de parcelamento:\n(1) meses\n(2) anos', function(periodo){
-                    let tempo = periodo
+                    // Converte o perído de parcelamento de string para número
+                    let tempo = Number(periodo)
 
                     entradaDeDados.question('Digite o tempo de parcelamento:', function(meses){
-                        let parcela = meses
+                        // Converte os meses de parcelamento de string para número
+                        let parcela = Number(meses)
 
                     // validação da entrada de dados 
-                    if (nomeCliente == '' || nomeProduto == '' || valorCompra == '' || taxaJuros == '' || parcela == ''){
+                    if (nomeCliente == '' || nomeProduto == ''){
                         console.log('Erro: Preencha todos os campos antes de continuar')
 
                     }else if(!isNaN(nomeCliente)){
@@ -49,9 +53,13 @@ entradaDeDados.question('Digite o nome do(a) cliente: ', function(nome){
                     }else if(valorCompra <= 0 || taxaJuros <= 0 || parcela <= 0){
                         console.log('Erro: Os campo não aceitam números menores ou iguais que 0 ')
                         
+                    }else if (isNaN(tempo) || tempo != 1 && tempo != 2){
+                        console.log('Erro: O campo aceita somente o número 1 (meses) ou número 2 (anos)')
+
                     }else{
 
-                        if (tempo == 2){
+                        // desafio bem legal - tempo de parcela por anos
+                        if (tempo === 2){
                             parcela = parcela * 12
                             taxaJuros = taxaJuros / 12
                         }
@@ -69,7 +77,7 @@ entradaDeDados.question('Digite o nome do(a) cliente: ', function(nome){
 
                         // estrutura da saída de dados
                         console.log(`\n******************* Viva Moda *******************\nMuito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}.\nA compra do produto ${nomeProduto}, tem um valor de: R$ ${valorCompra}.\nA sua compra será parcelada em ${parcela} vezes e o Sr(a) pagará: R$ ${montante.toFixed(2)}.\nO acréscimo realizado ao valor de: R$ ${valorCompra} será de R$ ${acrescimo.toFixed(2)}.\n\nMuito obrigado por escolher a Viva Moda.\n*******************************************************`)
-                        }
+                    }
                     })   
                 }) 
             })
